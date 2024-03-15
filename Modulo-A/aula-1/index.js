d8f7aac7df200db28fb7be8e9fa5ad2d34bdcdd4
BellKan <coisasaleatoria338@gmail.com>
2024-03-15 11:51:14 -0300

    curso

---
+++ Modulo-A/aula-1/index.js
@@ -0,0 +1,4 @@
+window.alert('minha primeira mensagem');
+window.confirm('Está gostando de JS?');
+window.prompt('Qual é seu nome?')
+
---
+++ Modulo-B/aula-2/index.js
@@ -0,0 +1,3 @@
+//meu segundo código
+var nome = window.prompt('Qual é o seu nome?');//pergunta o nome!!
+window.alert('Seja bem vindo (a), ' + nome +'!');
