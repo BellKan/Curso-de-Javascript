27990f882d5ebdb35b2e3a24c83641e20a330e02
BellKan <coisasaleatoria338@gmail.com>
2024-03-18 10:46:20 -0300

    aulas 8 e 9

---
+++ Modulo-D/aula-8/index.js
@@ -0,0 +1,7 @@
+var vel = 100
+
+console.log(`A velocidade do seu carro ultrapassou o limite de velocidade da via! Sua velocidade é de ${vel} Km/h`)
+
+if (vel > 60){
+    console.log(`chegarar em sua casa uma multa no valor de R$ 350,00`)
+}
