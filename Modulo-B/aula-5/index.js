2442bb26fdf441c727dae505f5b6b4add60c91c4
BellKan <coisasaleatoria338@gmail.com>
2024-03-17 21:36:50 -0300

    aula-5

---
+++ Modulo-B/aula-5/index.js
@@ -0,0 +1,8 @@
+
+var corpo = window.document.body
+var p1 = window.document.getElementsByTagName('p')[1]
+/*var d = window.document.getElementById('msg')
+d.style.background = 'green'*/
+//d.innerText = 'estou aguardando...'
+
+document.write(p1.innerText)
