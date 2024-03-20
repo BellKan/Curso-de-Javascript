2a46d885728041488bb9dd44be40e2fc821847e0
BellKan <coisasaleatoria338@gmail.com>
2024-03-19 22:15:51 -0300

    aula 10/12

--- Modulo-D/aula-10/index.js
+++ Modulo-D/aula-10/index.js
@@ -1,2 +1,13 @@
 
-
+var idade = 70
+if (idade < 16) {
+    console.log('não vota!')
+} else if (idade >= 16 && idade <= 18 || idade > 65) {
+    
+        console.log('voto opcional!')
+    } else if (idade > 18 && idade <=60) {
+        
+            console.log('voto obrigatorio!')
+        
+    
+}
