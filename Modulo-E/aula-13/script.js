201b348c1744232d26799cd05b73b4b8cdbfd658
BellKan <coisasaleatoria338@gmail.com>
2024-03-23 13:19:23 -0300

    aula-13/14

---
+++ Modulo-E/aula-13/script.js
@@ -0,0 +1,24 @@
+/*var c = 1
+while (c <= 1000) {
+    console.log(`${c} te amor mil milhões`)
+    c++
+}*/
+
+var c = 1
+
+do{
+    console.log(`${c} te amor mil milhões`)
+    c++
+} while (c <= 1000)
+
+
+
+
+
+
+/*console.log('tudo bem!')
+console.log('tudo bem!')
+console.log('tudo bem!')
+console.log('tudo bem!')
+console.log('tudo bem!')
+console.log('tudo bem!')*/
