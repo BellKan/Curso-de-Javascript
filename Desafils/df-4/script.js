19cc97e18f32442b58186692b2feed456bf6fc7e
BellKan <coisasaleatoria338@gmail.com>
2024-03-24 16:23:55 -0300

    df-4

--- Desafils/df-4/script.js
+++ Desafils/df-4/script.js
@@ -1,9 +1,36 @@
 
 function contar() {
-var inicio = document.getElementById('ini')
-var fim = document.getElementById('fim')
-var passo = document.getElementById('pas')
+let ini = document.getElementById('txti')
+let fim = document.getElementById('txtf')
+let passo = document.getElementById('txtp')
 
-var res = 
+let res = document.getElementById('res')
 
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = '[ERRO] Está faltando dados! Tente novamente!'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (p <= 0){
+            window.alert('[ERRO] Dados incorreto!  Considerando PASSO 1')
+            p = 1
+        }
+
+        if (i < f) {
+            //contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} `
+            }
+        } else {
+            //contagem decrescente
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} `
+            }
+        }
+        
+
+    }
 }
