e423dc170cd5354981cc09fbe723e7b2798d8e39
BellKan <coisasaleatoria338@gmail.com>
2024-03-25 18:43:57 -0300

    df-6 final do curso

--- Desafils/df-6/script.js
+++ Desafils/df-6/script.js
@@ -22,8 +22,48 @@ function inlista(n, l) {
 
 function adicionar() {
     if (isNumero(num.value) && !inlista(num.value, valores)){
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} Adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou ja encotrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+
+        let maior = valores[0]
+        let menor = valores[0]
+
+        let soma = 0
+        let media = 0
+
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+                if (valores[pos] < menor) {
+                    menor = valores[pos]
+                }
+            }
+        }
+
+        media = soma / tot
+
+        res.innerHTML = ''
+        res.innerHTML += `<p class="p1">Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p class="p1">O maior valor informado foi ${maior}!</p>`
+        res.innerHTML += `<p class="p1">O menor valor informado foi ${menor}!</p>`
+        res.innerHTML += `<p class="p1">Somando todos os valores temos ${soma}</p>`
+        res.innerHTML += `<p class="p1">A média dos valores digitado é ${media}!</p>`
     }
 }
